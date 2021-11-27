f45f9971a0f0f5060f971d07805eb341a4d47eca
SPRAVEDLIVO <misha.domanov@mail.ru>
2021-11-27 18:29:18 +0300

    j

--- templates.py
+++ templates.py
@@ -1,3 +1,17 @@
+"""
+templates is list of Template
+Template structure:
+- name: name of template
+- greetings: list of strings, first message to send to delivery subject
+- text: text to send after message has been read/replied by delivery subject
+- review: review message to send to delivery subject
+- picture: can be [PATH, any, math or "exact_type"], NOTE: this will be replaced with "обычный" if delivery type is "ДВОЙНОЕ"
+    PATH: location to picture relative to pictures folder (e.g. "picture.png", "picture.png" must exist in pictures folder)
+    any: any image from location
+    match: match image to delivery type
+    exact_type: string that represents exact type (обычный, конский etc.)
+"""
+
 templates = [
     {
         "name": "Вежливая доставка",
@@ -7,6 +21,6 @@ templates = [
         ],
         "text": "На ваше имя оформлен заказ: член, половой, {product_name}. Доставка осуществляется непосредственно в личные сообщения.Примите и распишитесь. С уважением, почтовая интернет-служба",
         "review": "Оцените, пожалуйста, качество доставки по 10-бальной шкале.",
-        "picture": "any",
+        "picture": "any"
     }
 ]
